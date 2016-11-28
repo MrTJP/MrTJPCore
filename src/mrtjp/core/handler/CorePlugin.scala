d86617ef5c2d9f09ed1905d7720f273e2e5ab965
covers1624 <laughlan.cov@internode.on.net>
2016-11-28 19:46:37 +1030

    Remove refs to the DepLoader

--- src/mrtjp/core/handler/CorePlugin.scala
+++ src/mrtjp/core/handler/CorePlugin.scala
@@ -7,7 +7,6 @@ package mrtjp.core.handler
 
 import java.util.{Map => JMap}
 
-import codechicken.core.launch.DepLoader
 import net.minecraftforge.fml.relauncher.{IFMLCallHook, IFMLLoadingPlugin}
 
 class CorePlugin extends IFMLLoadingPlugin with IFMLCallHook
@@ -24,7 +23,6 @@ class CorePlugin extends IFMLLoadingPlugin with IFMLCallHook
 
     override def call():Void =
     {
-        DepLoader.load()
         null
     }
 }
