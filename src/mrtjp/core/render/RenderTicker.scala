80278680615215a41580e7bc7d653e0076d683c1
covers1624 <laughlan.cov@internode.on.net>
2016-12-19 22:37:57 +1030

    Build stuff.

--- src/mrtjp/core/render/RenderTicker.scala
+++ src/mrtjp/core/render/RenderTicker.scala
@@ -1,31 +1,31 @@
-/*
- * Copyright (c) 2014.
- * Created by MrTJP.
- * All rights reserved.
- */
-package mrtjp.core.render
-
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import net.minecraftforge.fml.common.gameevent.TickEvent
-import net.minecraftforge.fml.common.gameevent.TickEvent.Phase
-
-object RenderTicker
-{
-    var renderTime = 0
-    var renderFrame = 0.0f
-
-    @SubscribeEvent
-    def clientTick(event:TickEvent.ClientTickEvent)
-    {
-        if (event.phase == Phase.END) renderTime += 1
-    }
-
-    @SubscribeEvent
-    def renderTick(event:TickEvent.RenderTickEvent)
-    {
-        if (event.phase == Phase.START) renderFrame = event.renderTickTime
-    }
-
-    def getRenderFrame = renderFrame
-    def getRenderTime = renderTime+renderFrame
-}
+///*
+// * Copyright (c) 2014.
+// * Created by MrTJP.
+// * All rights reserved.
+// */
+//package mrtjp.core.render
+//
+//import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+//import net.minecraftforge.fml.common.gameevent.TickEvent
+//import net.minecraftforge.fml.common.gameevent.TickEvent.Phase
+//
+//object RenderTicker
+//{
+//    var renderTime = 0
+//    var renderFrame = 0.0f
+//
+//    @SubscribeEvent
+//    def clientTick(event:TickEvent.ClientTickEvent)
+//    {
+//        if (event.phase == Phase.END) renderTime += 1
+//    }
+//
+//    @SubscribeEvent
+//    def renderTick(event:TickEvent.RenderTickEvent)
+//    {
+//        if (event.phase == Phase.START) renderFrame = event.renderTickTime
+//    }
+//
+//    def getRenderFrame = renderFrame
+//    def getRenderTime = renderTime+renderFrame
+//}
